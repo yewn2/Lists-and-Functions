3a5ecbee5fa1c94f01520b2612ac4e9c01e5bae3
yewn2 <yewn2@middleton.school.nz>
2024-02-14 13:54:22 +1300

    pickUp version 1

---
+++ MGS Childcare/02_pick_up_v1.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def pickUp(roll):
+    name = input("What child would you like to pick up? ")
+    for name in roll:
+        while name not in roll:
+            name = input("What child would you like to pick up? ")
+        if name in roll:
+            print("Your child: ")
+            print("".join(name))
+            print("is ready for pickup!")
+            sys.exit()
+
+
+child_list = ["John", "Jimmy", "Maxwell Francis Longshoremen"]
+
+pickUp(child_list)
