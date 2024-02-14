7575322c89e11b10786137a9040a9b43fda077a0
yewn2 <yewn2@middleton.school.nz>
2024-02-14 14:03:32 +1300

    base component version 3

---
+++ MGS Childcare/00_MGS_Childcare_v3.py
@@ -0,0 +1,72 @@
+import sys
+
+
+def menu():
+    choice = 0
+    child_list = []
+    while choice != 5:
+        print("---------------------------------------------------------------"
+              "--------")
+        print("Welcome to MGS Childcare")
+        print("What would you like to do? Please choose one of the items below")
+        print("---------------------------------------------------------------"
+              "--------")
+        print()
+        print("1 Drop off a child")
+        print("2 Pick up a child")
+        print("3 Calculate cost")
+        print("4 Print roll")
+        print("5 Exit the system")
+        print()
+        choice = number_checker("Enter your choice (number between 1 and 5): ")
+        print()
+        if choice == 1:
+            dropOff(child_list)
+        elif choice == 2:
+            pickUp()
+        elif choice == 3:
+            calcCost()
+        elif choice == 4:
+            printRoll()
+        else:
+            print("Goodbye")
+
+
+def dropOff(roll):
+    roll.append(input("What is your child's name? "))
+    last_item = len(roll)
+    second_to_last = last_item - 1
+    child_name = roll[second_to_last:last_item]
+    print()
+    print("Your child: ")
+    print("".join(child_name))
+    print("has been added to the roll")
+    print()
+    return roll
+
+
+def pickUp():
+    ...
+
+
+def calcCost():
+    ...
+
+
+def printRoll():
+    ...
+
+
+def number_checker(question):
+    error = "\nSorry, you must enter an integer\n"
+    number = ""
+    while not number:
+        try:
+            number = int(input(question))
+            return number
+        except ValueError:
+            print(error)
+
+
+# main program
+menu()
