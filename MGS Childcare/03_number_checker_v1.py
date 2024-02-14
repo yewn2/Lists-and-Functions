94e14bad5cf9bbd5a570663871d5120e325ae4c0
yewn2 <yewn2@middleton.school.nz>
2024-02-14 13:57:32 +1300

    number checker (copied from previous projects)

---
+++ MGS Childcare/03_number_checker_v1.py
@@ -0,0 +1,9 @@
+def integer_checker(question):
+    error = "\nSorry, you must enter an integer\n"
+    number = ""
+    while not number:
+        try:
+            number = int(input(question))
+            return number
+        except ValueError:
+            print(error)
