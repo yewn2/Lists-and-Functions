1ded82a546fbd92b41bddf1b778bfeef02753687
yewn2 <yewn2@middleton.school.nz>
2024-02-16 11:13:19 +1300

    base component version 4

---
+++ Silent Auction/00_Silent_Auction_v4.py
@@ -0,0 +1,56 @@
+"""
+Silent Auction version 4
+This is the fourth version of the base component
+New function auction added
+"""
+
+
+def main():
+    auction_item = input("What is the auction for? ")
+    reserve_price = number_checker("What is the reserve price? ")
+    print(formatter("=", f"\nThe auction for the {auction_item} "
+                         f"has started!\n"))
+    auction(reserve_price, auction_item)
+
+
+def formatter(symbol, text):
+    sides = symbol * 3
+    formatted_text = f"{sides} {text} {sides}"
+    top_bottom = symbol * len(formatted_text)
+    return f"{top_bottom}\n{formatted_text}\n{top_bottom}"
+
+
+def number_checker(question):
+    error = "\nSorry, you must enter an integer\n"
+    number = ""
+    while not number:
+        try:
+            number = float(input(question))
+            return number
+        except ValueError:
+            print(error)
+
+
+def auction(reserve, item):
+    highest = 0
+    while highest >= 0:
+        print(f"Highest bid: ${highest:.2f}")
+        bid = number_checker("Your bid: $")
+        while bid <= highest:
+            if bid < 0:
+                break
+            print("Please enter a higher bid.")
+            bid = number_checker("Your bid: $")
+        if bid >= 0:
+            highest = bid
+        else:
+            break
+    if highest >= reserve:
+        print(formatter("|", f"The auction for the {item} "
+                             f"finished with a bid of ${highest:.2f}"))
+    else:
+        print(formatter("/", f"The {item} did not sell."))
+
+
+# main program
+main()
