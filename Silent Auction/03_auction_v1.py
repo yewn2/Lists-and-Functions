a6a79e209ad4d75ec4ed2163b6a52e9fbbdd8cc1
yewn2 <yewn2@middleton.school.nz>
2024-02-16 11:13:08 +1300

    auction version 1

--- Silent Auction/03_auction_v1.py
+++ Silent Auction/03_auction_v1.py
@@ -1,7 +1,19 @@
-def auction(reserve):
+def auction(reserve, item):
     highest = 0
     while highest >= 0:
         print(f"Highest bid: ${highest:.2f}")
-        highest = float(input("Your bid: $"))
+        bid = float(input("Your bid: $"))
+        while bid <= highest:
+            if bid < 0:
+                break
+            print("Please enter a higher bid.")
+            bid = float(input("Your bid: $"))
+        if bid >= 0:
+            highest = bid
+        else:
+            break
     if highest >= reserve:
-        return True
+        print(f"The auction for the {item} finished with a bid "
+              f"of ${highest:.2f}")
+    else:
+        print(f"The {item} did not sell.")
