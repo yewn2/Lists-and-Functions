b295b5a9eceeb9f0f063869e5a71aeeaaad0db4b
yewn2 <yewn2@middleton.school.nz>
2024-02-16 22:04:48 +1300

    base component version 1

--- Taxi Trips/00_Taxi_Trips_v1.py
+++ Taxi Trips/00_Taxi_Trips_v1.py
@@ -7,7 +7,7 @@ No functions except main are written yet
 
 def main():
     driver = input("What is the driver's name? ")
-    trips, tot_time, avg_time, income, avg_cost = trip(driver)
+    trips, tot_time, avg_time, income, avg_cost = trip()
     print("="*30)
     print(f"Driver {driver} had {trips} trips totalling {tot_time} minutes")
     print(f"The average time of all trips was {avg_time:.1f} minutes")
