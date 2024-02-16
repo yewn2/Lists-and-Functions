f3ec5f55d74dd8054fd44e21d64bbc5c9374ea8d
yewn2 <yewn2@middleton.school.nz>
2024-02-16 22:01:32 +1300

    trip version 1

---
+++ Taxi Trips/01_trip_v1.py
@@ -0,0 +1,22 @@
+"""
+Trip function v1
+"""
+
+
+def trip():
+    new_trip = True
+    trips_taken = 0
+    all_time = 0
+    average_time = 0
+    all_income = 0
+    average_cost = 0
+    while new_trip:
+        trips_taken += 1
+        time_taken = float(input("How many minutes did the trip take? "))
+        cost = 10 + (time_taken * 2)
+        print(f"This trip cost ${cost:.2f}")
+        all_time += time_taken
+        all_income += cost
+    average_cost = all_income / trips_taken
+    average_time = all_time / trips_taken
+    return trips_taken, all_time, average_time, all_income, average_cost
