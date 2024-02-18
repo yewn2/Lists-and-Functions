1f436f821014ecfb70e36e2195081e13ce347279
yewn2 <yewn2@middleton.school.nz>
2024-02-18 13:21:48 +1300

    staff_absence version 1

--- Work Absences/01_staff_absence_v1.py
+++ Work Absences/01_staff_absence_v1.py
@@ -4,12 +4,15 @@ Staff absence list creator v1
 
 
 def staff_absence(lst, amount):
-    staff = "x"
+    staff, days = input("\nEnter the staff name and the number of days "
+                        "absent, separated by a comma and space.\n"
+                        "Enter the staff name as '$' and the days "
+                        "as '0' to finish. ").split(", ")
     while staff != "$":
+        lst.append(staff)
+        amount.append(days)
         staff, days = input("\nEnter the staff name and the number of days "
                             "absent, separated by a comma and space.\n"
                             "Enter the staff name as '$' and the days "
                             "as '0' to finish. ").split(", ")
-        lst.append(staff)
-        amount.append(days)
     return lst, amount
