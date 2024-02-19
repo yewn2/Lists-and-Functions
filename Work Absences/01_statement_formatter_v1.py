433bd18c207428a932f924970fb9875de837bb25
yewn2 <yewn2@middleton.school.nz>
2024-02-19 21:28:55 +1300

    statement_formatter v1

---
+++ Work Absences/01_statement_formatter_v1.py
@@ -0,0 +1,11 @@
+"""
+Statement formatter v1
+Copied from project: Silent Auction
+"""
+
+
+def statement_formatter(symbol, text):
+    sides = symbol * 3
+    formatted_text = f"{sides} {text} {sides}"
+    top_bottom = symbol * len(formatted_text)
+    return f"{top_bottom}\n{formatted_text}\n{top_bottom}"
