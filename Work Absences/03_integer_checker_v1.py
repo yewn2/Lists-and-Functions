e2aa6cc8d4a1b07d9ce7cb3f94bf391db80fbd07
yewn2 <yewn2@middleton.school.nz>
2024-02-19 21:49:00 +1300

    integer_checker v1

---
+++ Work Absences/03_integer_checker_v1.py
@@ -0,0 +1,15 @@
+"""
+Integer checker v1
+Copied from project: MGS Childcare
+"""
+
+
+def integer_checker(question):
+    error = "\nSorry, you must enter an integer\n"
+    number = ""
+    while not number:
+        try:
+            number = int(input(question))
+            return number
+        except ValueError:
+            print(error)
