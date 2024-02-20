890c6ff9188396dcbbaacbc2ba0b7c5fcb7632b1
yewn2 <yewn2@middleton.school.nz>
2024-02-20 17:12:07 +1300

    end_program version 1

--- Speeding Motorists/04_end_program_v1.py
+++ Speeding Motorists/04_end_program_v1.py
@@ -7,6 +7,8 @@ This component takes the list of people and prints them out
 def end_program(lst, totl):
     totl_ppl = len(lst)
     print(f"Total fines: {totl_ppl}")
+    i = 1
     for names in lst:
-        print(f") Name: {names[0]}       Amount Over Limit: {names[1]}")
+        print(f"{i}) Name: {names[0]}       Amount Over Limit: {names[1]}")
+        i += 1
     print(f"Total amount of fines: ${totl}")
