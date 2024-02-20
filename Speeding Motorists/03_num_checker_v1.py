c73d63203d0093987c8475e02c0a240e02f7b31e
yewn2 <yewn2@middleton.school.nz>
2024-02-20 13:35:44 +1300

    num_checker version 1

---
+++ Speeding Motorists/03_num_checker_v1.py
@@ -0,0 +1,15 @@
+"""
+Number checker v1
+Copied from project: MGS Childcare
+"""
+
+
+def num_checker(question):
+    error = "\nSorry, you must enter an integer\n"
+    number = ""
+    while not number:
+        try:
+            number = int(input(question))
+            return number
+        except ValueError:
+            print(error)
