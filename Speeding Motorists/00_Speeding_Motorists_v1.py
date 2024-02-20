94cb300729a9211e811c5ce1504d6e8e4a372017
yewn2 <yewn2@middleton.school.nz>
2024-02-20 13:25:02 +1300

    base component version 1

---
+++ Speeding Motorists/00_Speeding_Motorists_v1.py
@@ -0,0 +1,32 @@
+"""
+Base component version 1
+No components written yet except main
+"""
+
+
+def main():
+    speeds = []
+    speeder(speeds)
+    end_program(speeds)
+
+
+def speeder(lst):
+    person = "" 
+    arrest(person)
+
+
+def arrest(name):
+    ...
+
+
+def num_checker(question):
+    ...
+
+
+def end_program(lst):
+    ...
+
+
+# main program
+if __name__ == '__main__':
+    main()
