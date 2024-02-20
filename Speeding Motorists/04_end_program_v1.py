e201294e7a9becac4925243d79d5a0cadf4bcd9d
yewn2 <yewn2@middleton.school.nz>
2024-02-20 14:16:03 +1300

    end_program version 1

---
+++ Speeding Motorists/04_end_program_v1.py
@@ -0,0 +1,12 @@
+"""
+End Program version 1
+This component takes the list of people and prints them out
+"""
+
+
+def end_program(lst, totl):
+    totl_ppl = len(lst)
+    print(f"Total fines: {totl_ppl}")
+    for names in lst:
+        print(f") Name: {names[0]}       Amount Over Limit: {names[1]}")
+    print(f"Total amount of fines: ${totl}")
